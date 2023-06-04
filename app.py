5333e7541457ef6bdc72f2d81d16ced78cfaa577
yayajmliu <yayajmliu@tencent.com>
2023-06-04 19:08:31 +0800

    增加 pywebio 框架，https://github.com/pywebio/PyWebIO/blob/dev/README-zh.md

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import time
 from flask import Flask, render_template, request, Response
 
 from weread_2_notion import weread_2_notion
+from pywebio.platform.flask import webio_view
+from pywebio.input import input, TEXT
+from pywebio.output import put_text
+
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -22,6 +27,22 @@ def home():
 
     return render_template('wereadtonotion.html')
 
+
+def test():
+    height = input("Notion tken(cm)：", type=TEXT)
+    weight = input(" 微信读书 cookies(kg)：", type=TEXT)
+
+    # BMI = weight / (height / 100) ** 2
+
+    top_status = [(14.9, '极瘦'), (18.4, '偏瘦'),
+                  (22.9, '正常'), (27.5, '过重'),
+                  (40.0, '肥胖'), (float('inf'), '非常肥胖')]
+
+    put_text('这是输出')
+
+
+app.add_url_rule('/test', 'webio_view', webio_view(test),
+                 methods=['GET'])  # need GET,POST and OPTIONS methods
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
