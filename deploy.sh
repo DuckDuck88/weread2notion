e6b18260b43cbf54b3ee3a1d0cbe4d39fc8ab115
yayajmliu <yayajmliu@tencent.com>
2023-06-05 21:02:10 +0800

    增加启动脚本

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 获取应用版本参数
+version=$1
+
+# 默认应用版本
+default_version="latest"
+
+# 如果没有指定版本参数，则使用默认版本
+if [ -z "$version" ]; then
+  version=$default_version
+fi
+
+# 构建Docker镜像
+docker build -t weread-to-notion:$version .
+
+# 启动容器
+docker run -p 8000:6000 weread-to-notion:$version
