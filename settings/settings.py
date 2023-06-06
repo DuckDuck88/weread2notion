871b8e43686663d5256f2796303714a755561587
yayajmliu <yayajmliu@tencent.com>
2023-06-06 12:45:38 +0800

    x

--- settings/settings.py
+++ settings/settings.py
@@ -1,4 +1,4 @@
-NOTION_TOKEN = 'secret_kZMZkH95SLspBpZb0OPD49pTZ2JuGL2z9DeyoHpe3DG'
+NOTION_TOKEN = 'xxxxxxxxxxx'
 WEREAD_COOKIE = 'wr_fp=2533447432; wr_gid=295032143; wr_vid=23683664; wr_pf=0; wr_rt=web%40QxPpLky5s9q8MDFU8TK_AL; wr_localvid=fb3325d071696250fb37ae2; wr_name=%E8%AF%B6%E9%B8%AD; wr_avatar=https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FXpfWjDr8uAYiagxaSibFmnMh2LUJXTQrjJ8z2ve6X8J3w2CI79YSvQwic1icyHUoHVgoccckuoLxlt1cQOD87tZLfQ%2F132; wr_gender=1; RK=Ou2xAkMpZ9; ptcz=6d556d39aaf44f088fec74b97d2942db2ea837b96f5c8c603ddacec2b5e1fd45; uin=o1307317886; wr_skey=t1Gw1zJp'
 DATABASE_ID = 'd92bb4b8434745baa2061caf67d6ef7a'
 
