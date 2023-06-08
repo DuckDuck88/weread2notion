778c7ace02d00464ec293517b8af01663a349bf9
yaya <1307317886@qq.com>
2023-06-08 14:45:58 +0800

    edit process

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 bind = '0.0.0.0:80'  # 绑定的IP地址和端口号
 # bind = '127.0.0.1:6000'  # 绑定的IP地址和端口号
-workers = 4  # 工作进程的数量
+workers = 1  # 工作进程的数量
 worker_class = 'gevent'  # 使用gevent作为工作进程的类别
 timeout = 12000  # 超时时间，单位为秒
 accesslog = '/var/log/weread'  # 访问日志的文件路径，'-' 表示输出到标准输出
